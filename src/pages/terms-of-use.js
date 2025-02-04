0e74fedfd0c0dcd57938e4c5b521f8ed9f2dbb8b
arjunTltech <arjunntltechnologies@gmail.com>
2025-02-04 11:00:23 +0530

    added privacy, terms, and testimonials banners

--- src/pages/terms-of-use.js
+++ src/pages/terms-of-use.js
@@ -4,13 +4,14 @@ import Header from "@/components/Header/Header";
 import Layout from "@/components/Layout/Layout";
 import PageHeader from "@/components/Reuseable/PageHeader";
 import TermsOfUsePolicy from "@/components/TermsOfUse/TermsOfUsePolicy";
+import bg_terms from "@/images/backgrounds/terms-banner-image.jpg";
 import React from "react";
 
 const TermsOfUse = () => {
   return (
     <Layout pageTitle="Terms Of Use Policy">
       <Header />
-      <PageHeader title="Terms and Conditions & Disclaimer" />
+      <PageHeader title="Terms and Conditions & Disclaimer" bgImage={bg_terms} />
       <TermsOfUsePolicy />
       {/* <ContactPage /> */}
       <CtaOne />
