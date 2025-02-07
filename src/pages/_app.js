2e3ef14e77a35fa5ae1eb564e3bb1168145fecc6
ali-tltech <murthasaali.tltechnologies@gmail.com>
2025-02-07 09:46:35 +0530

    Updated g tag

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,53 +1,99 @@
-    // <======================================== NOTES START ==============================================>
+// <======================================== NOTES START ==============================================>
 
-  // Libraries used :   "react-bootstrap" for css
-  // Read the documentaion in their respective sites for the above mentioned libraries before making changes in the code.
-  // To run the code: npm run dev
-  // First install all dependencies :- npm intsall
-  // Then run the code :- npm run dev
-  // to build code :-  npm run build
-  // created date : 22-1-2025 || created by : Arjun N  || module : 1 ||
-  // modified date : 1/2/2025 || modified by : Arjun N || module : 1 ||
-  // modified date : 1/2/2025 || modified by : Arjun N  || module : 1 ||
-  // Technical summary(Pre-setups) created date/by :  Arjun N || Ali
-  // Domain :   || 
-  // Hosting :   ||
-  // SSL :   ||
-  // Database :  ||
+// Libraries used: "react-bootstrap" for styling  
+// Read the documentation on their official websites before making changes.  
 
-  // Phase summary :   || created date/by :  Arjun N  ||
-  // Phase 1 :  Git hub creation  ||  
-  // Phase 2 :  Development/Main page creation ||
-  // Phase 3 :  Production  ||
+// To run the code: npm run dev  
+// First, install all dependencies: npm install  
+// Then run the code: npm run dev  
+// To build the project: npm run build  
 
-  // <======================================== NOTES END ==============================================>
+// Created date: 22-01-2025 || Created by: Arjun N || Module: 1 ||  
+// Modified date: 01-02-2025 || Modified by: Arjun N || Module: 1 ||  
 
+// Technical Summary (Pre-setups)  
+// Created by: Arjun N, Ali  
+// Domain: [Add Domain Here]  
+// Hosting: [Add Hosting Details Here]  
+// SSL: [Add SSL Details Here]  
+// Database: [Add Database Details Here]  
 
-import ContextProvider from "@/context/ContextProvider";
-import "@/vendors/animate/animate.min.css";
-import "@/vendors/animate/custom-animate.css";
-import "@/vendors/fontawesome/css/all.min.css";
-import "@/vendors/oslim-icons/style.css";
-import "@/vendors/reey-font/stylesheet.css";
-import "@/vendors/the-sayinistic-font/stylesheet.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "node_modules/swiper/swiper-bundle.min.css";
-import "react-modal-video/css/modal-video.css";
-import "jarallax/dist/jarallax.css";
-import "tiny-slider/dist/tiny-slider.css";
-import { Toaster } from 'react-hot-toast';
+// Phase Summary: || Created by: Arjun N ||  
+// Phase 1: GitHub Repository Setup  
+// Phase 2: Development / Main Page Creation  
+// Phase 3: Production  
 
-// extra css
-import "@/styles/style.css";
-import "@/styles/responsive.css";
+// <======================================== NOTES END ==============================================>
 
-const MyApp = ({ Component, pageProps }) => {
-  return (
-    <ContextProvider>
-      <Toaster position="bottom-center" reverseOrder={false} />
-      <Component {...pageProps} />
-    </ContextProvider>
-  );
-};
-
-export default MyApp;
+  import { useEffect } from "react";
+  import { useRouter } from "next/router";
+  import Script from "next/script";
+  import ContextProvider from "@/context/ContextProvider";
+  
+  // Google Analytics Tracking ID
+  const GA_TRACKING_ID = "G-56XPSKHF5L"; // Replace with your actual Google Analytics ID
+  
+  // Vendor CSS
+  import "@/vendors/animate/animate.min.css";
+  import "@/vendors/animate/custom-animate.css";
+  import "@/vendors/fontawesome/css/all.min.css";
+  import "@/vendors/oslim-icons/style.css";
+  import "@/vendors/reey-font/stylesheet.css";
+  import "@/vendors/the-sayinistic-font/stylesheet.css";
+  import "bootstrap/dist/css/bootstrap.min.css";
+  import "swiper/swiper-bundle.min.css";
+  import "react-modal-video/css/modal-video.css";
+  import "jarallax/dist/jarallax.css";
+  import "tiny-slider/dist/tiny-slider.css";
+  
+  // Extra CSS
+  import "@/styles/style.css";
+  import "@/styles/responsive.css";
+  
+  import { Toaster } from "react-hot-toast";
+  
+  const MyApp = ({ Component, pageProps }) => {
+    const router = useRouter();
+  
+    useEffect(() => {
+      const handleRouteChange = (url) => {
+        window.gtag("config", GA_TRACKING_ID, {
+          page_path: url,
+        });
+      };
+      router.events.on("routeChangeComplete", handleRouteChange);
+      return () => {
+        router.events.off("routeChangeComplete", handleRouteChange);
+      };
+    }, [router.events]);
+  
+    return (
+      <ContextProvider>
+        {/* Google Analytics Script */}
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+        
+        <Toaster position="bottom-center" reverseOrder={false} />
+        <Component {...pageProps} />
+      </ContextProvider>
+    );
+  };
+  
+  export default MyApp;
+  
