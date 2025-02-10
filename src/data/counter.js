062aa2567b1b4dd6110e523e37d5ea2f7c095684
arjunTltech <arjunntltechnologies@gmail.com>
2025-02-10 15:26:49 +0530

    updated number

--- src/data/counter.js
+++ src/data/counter.js
@@ -3,24 +3,24 @@ export const counterOne = [
     id: 1,
     icon: "icon-scrum",
     count: 8586,
-    text: "Completed Cases",
+    text: "Resolved Partner Cases",
   },
   {
     id: 2,
     icon: "icon-business-idea",
     count: 697,
-    text: "Expert Consultant",
+    text: "Audit Man-days",
   },
   {
     id: 3,
     icon: "icon-recruit",
     count: 448,
-    text: "Finance Advices",
+    text: "Coaching Man-days ",
   },
   {
     id: 4,
     icon: "icon-customer-review",
     count: 2887,
-    text: "Happy Customers",
+    text: "Transformation Man-days",
   },
 ];
